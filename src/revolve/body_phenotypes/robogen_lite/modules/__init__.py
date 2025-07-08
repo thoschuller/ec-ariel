d990c9e40beec45ebcaac3cdbeac07d740b88981
JMDM <jmo.880@gmail.com>
2025-07-08 08:34:00 +0200

    Add: instances enum

--- src/revolve/body_phenotypes/robogen_lite/modules/__init__.py
+++ src/revolve/body_phenotypes/robogen_lite/modules/__init__.py
@@ -1,11 +1,26 @@
 """Robogen Lite Modules."""
 
+# Standard library
+from enum import Enum
+
+# Local libraries
 from .brick import BrickModule
 from .core import CoreModule
 from .hinge import HingeModule
+
+
+class ModuleTypeInstances(Enum):
+    """Enum for module types."""
+
+    CORE = CoreModule
+    BRICK = BrickModule
+    HINGE = HingeModule
+    DEAD = None
+
 
 __all__ = [
     "BrickModule",
     "CoreModule",
     "HingeModule",
+    "ModuleTypeInstances",
 ]
