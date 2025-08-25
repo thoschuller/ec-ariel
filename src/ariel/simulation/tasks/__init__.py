a22ac1353c2894fd93574b17b399e8eba8b77ee4
A-lamo <a.r.ferencz@student.vu.nl>
2025-08-25 11:17:56 +0200

    Add: __all__ for fitness functions and tasks.

--- src/ariel/simulation/tasks/__init__.py
+++ src/ariel/simulation/tasks/__init__.py
@@ -3,4 +3,10 @@ from .targeted_locomotion import distance_to_target_ff
 from .turning_in_place import turning_in_place_ff
 
 
-
+__all__ = [
+    "xy_displacement_ff",
+    "x_speed_ff",
+    "y_speed_ff",
+    "distance_to_target_ff",
+    "turning_in_place_ff",
+]
