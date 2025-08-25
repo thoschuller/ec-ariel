0d4e605614266df9e7155026a792304119895096
A-lamo <a.r.ferencz@student.vu.nl>
2025-08-25 12:18:43 +0200

    Add: Task -> Fitness function map.

--- src/ariel/simulation/tasks/__init__.py
+++ src/ariel/simulation/tasks/__init__.py
@@ -2,6 +2,16 @@ from .gate_learning import xy_displacement_ff, x_speed_ff, y_speed_ff
 from .targeted_locomotion import distance_to_target_ff
 from .turning_in_place import turning_in_place_ff
 
+tasks = [
+    "Gate Learning",
+    "Targeted Locomotion",
+    "Turning In Place"]
+
+_task_fitness_function_map_ = {
+    "Gate Learning": [xy_displacement_ff, x_speed_ff, y_speed_ff],
+    "Targeted Locomotion": [distance_to_target_ff],
+    "Turning In Place": [turning_in_place_ff],
+}
 
 __all__ = [
     "xy_displacement_ff",
