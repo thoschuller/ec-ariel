739f7f887df3a17efe866143f971d4c41fc9d839
A-lamo <a.r.ferencz@student.vu.nl>
2025-08-26 12:36:36 +0200

    Edit: Solved the dataflow issues by adding a final output node.

--- src/ariel/viz/gui/viewer/test_ea.py
+++ src/ariel/viz/gui/viewer/test_ea.py
@@ -39,8 +39,6 @@ class EAParameters(Node):
         self.set_output_value("PopSizeOut", pop_size)
         self.set_output_value("GenerationsOut", generations)
 
-
-
 # ----------------------------
 # EA Run Node
 # ----------------------------
@@ -55,7 +53,7 @@ class EARun(Node):
         self.add_value_input("GenerationsOut")
 
         # Optional: Add an output for the result
-        self.add_label_output("Result")
+        self.add_label_output("Best Individual")
 
     def evaluate(self, values: dict):
 
@@ -64,7 +62,7 @@ class EARun(Node):
         pop_size = values["PopSizeOut"]
         generations = values["GenerationsOut"]
 
-        console.log(f"Running EA with population={pop_size}, generations={generations}")
+        console.log(f"Running EA -> with population={pop_size}, generations={generations}")
 
         # Build initial population
         population_list = [create_individual() for _ in range(int(pop_size))]
@@ -93,10 +91,19 @@ class EARun(Node):
         console.log(f"[red]Worst:[/red] {worst}")
 
         # Save result to output so other nodes can use it
-        self.set_output_value("Result", best)
-        self.result = best  # store for retrieval after dialog.exec()
+        self.set_output_value("Best Individual", best)
+        self.result = best 
 
         return best
+
+class EAResult(Node):
+    code = 2
+
+    def create(self):
+        self.title = "EA Result - Best Individual"
+        self.add_value_input("Result")  # Input from EARun
+
+        print('The Best Individual from EARun is : ', self.get_entry("Result").calculate_value())
 
 # ----------------------------
 # Main
@@ -105,12 +112,55 @@ app = QApplication([])
 dialog = NodeEditorDialog()
 dialog.editor.available_nodes = {
     'EA Parameters': EAParameters,
-    'Run EA': EARun
+    'Run EA': EARun,
+    'EA Result' : EAResult
 }
 
 # Add output node instance
-ea_run_node = EARun()
-dialog.editor.output_node = ea_run_node
+ea_result_node = EAResult()
+dialog.editor.output_node = ea_result_node
 
 if dialog.exec():
-    print("Editor finished. Best individual:",  ea_run_node.evaluate(dialog.result))
+    print("Editor finished. Best individual:",  dialog.result)
+
+# ------------------------------------------------------------------------------
+# from QNodeEditor import Node
+# from PyQt5.QtWidgets import QApplication, QPushButton, QGraphicsProxyWidget
+# from QNodeEditor import NodeEditorDialog
+
+# class Addition(Node):
+#     code = 0
+
+#     def create(self):
+#         self.title = 'Add Two Numbers'
+#         self.add_label_output('Output')  # This line is super weird, but necessary to create an output
+
+#         self.add_value_input('Value 1')  # Add input (value_input means numerical input) for the first value
+#         self.add_value_input('Value 2')  # same for the second value
+
+#     def evaluate(self, values: dict):
+#         value1 = values['Value 1'] # Somehow evaluate() implicitly receives the input values as a dictionary
+#         value2 = values['Value 2']
+
+#         self.set_output_value('Output', value1 + value2) # This is where we actually set the value of the output
+#                                                          # that we created above with add_label_output()
+
+# class Output(Node):
+#     code = 1
+#     def create(self):
+#         self.title = 'Output Node'
+#         self.add_value_input('Out')  # You have to pay attention to the naming very carefully  
+
+# app = QApplication([])
+# dialog = NodeEditorDialog()
+# dialog.editor.available_nodes = {
+#     'Add': Addition,
+#     'Out': Output,
+# }
+# # Add output node instance we need to have at least one
+# # (I'm not sure if we can have multiple but it kind of seems like we can!)
+# output_node = Output()
+# dialog.editor.output_node = output_node
+
+# if dialog.exec():
+#     print("Editor finished...",  dialog.result)
