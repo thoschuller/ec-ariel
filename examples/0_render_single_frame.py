0aa8815d52767c293b10e3a9dfda7ca7a19fd4f0
Ioannis Grigoriadis <113774274+JohnGrigoriadis@users.noreply.github.com>
2025-08-28 13:15:02 +0200

    Update 0_render_single_frame.py

--- examples/0_render_single_frame.py
+++ examples/0_render_single_frame.py
@@ -12,6 +12,8 @@ Status:     Completed ✅
 import mujoco
 
 # Local libraries
+# If the following line returns an error, run this:
+from ariel.simulation.environments.simple_flat_world import SimpleFlatWorld
 from ariel.environments.simple_flat_world import SimpleFlatWorld
 from ariel.utils.renderers import single_frame_renderer
 
