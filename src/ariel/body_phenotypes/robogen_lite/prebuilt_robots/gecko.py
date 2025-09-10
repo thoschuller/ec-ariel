16178fb0f78718f901ab5dd7e0be6670d7607359
Ioannis Grigoriadis <113774274+JohnGrigoriadis@users.noreply.github.com>
2025-09-11 01:07:03 +0200

    Updated gecko.py
    added joints and rotated neck for better mobility

--- src/ariel/body_phenotypes/robogen_lite/prebuilt_robots/gecko.py
+++ src/ariel/body_phenotypes/robogen_lite/prebuilt_robots/gecko.py
@@ -2,7 +2,7 @@ from ariel.body_phenotypes.robogen_lite.config import ModuleFaces
 from ariel.body_phenotypes.robogen_lite.modules.brick import BrickModule
 from ariel.body_phenotypes.robogen_lite.modules.core import CoreModule
 from ariel.body_phenotypes.robogen_lite.modules.hinge import HingeModule
-
+import numpy as np
 
 def gecko():
     """
@@ -16,24 +16,43 @@ def gecko():
     neck = HingeModule(
         index=1,
     )
+    neck2 = HingeModule(
+        index=21,
+    )
+    neck.rotate(-90)
     abdomen = BrickModule(
         index=2,
     )
     spine = HingeModule(
         index=3,
     )
+    spine.rotate(90)
     butt = BrickModule(
         index=4,
     )
+    
     fl_leg = HingeModule(
         index=5,
     )
+    fl_leg.rotate(45)
+    fl_leg2 = HingeModule(
+        index=15,
+    )
+    fl_leg2.rotate(90)
+    
     fl_flipper = BrickModule(
         index=6,
     )
+    
     fr_leg = HingeModule(
         index=7,
     )
+    fr_leg.rotate(-45)
+    fr_leg2 = HingeModule(
+        index=17,
+    )
+    fr_leg2.rotate(90)
+    
     fr_flipper = BrickModule(
         index=8,
     )
@@ -72,18 +91,30 @@ def gecko():
         body=fl_leg.body,
         prefix="fl_leg",
     )
+    
     fl_leg.sites[ModuleFaces.FRONT].attach_body(
-        body=fl_flipper.body,
+        body=fl_leg2.body,
         prefix="fl_flipper",
     )
+    fl_leg2.sites[ModuleFaces.FRONT].attach_body(
+        body=fl_flipper.body,
+        prefix="fl_flipper2",
+    )
+    
     core.sites[ModuleFaces.RIGHT].attach_body(
         body=fr_leg.body,
         prefix="fr_leg",
     )
+    
     fr_leg.sites[ModuleFaces.FRONT].attach_body(
-        body=fr_flipper.body,
+        body=fr_leg2.body,
         prefix="fr_flipper",
     )
+    fr_leg2.sites[ModuleFaces.FRONT].attach_body(
+        body=fr_flipper.body,
+        prefix="fr_flipper2",
+    )
+    
     butt.sites[ModuleFaces.LEFT].attach_body(
         body=bl_leg.body,
         prefix="bl_leg",
