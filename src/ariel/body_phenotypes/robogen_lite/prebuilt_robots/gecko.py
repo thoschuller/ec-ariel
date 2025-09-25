81569a8af74d66236dd3cc22c0d629629a438b80
Jacopo-DM <jacopo.michele.dimatteo@gmail.com>
2025-09-25 07:51:49 +0200

    Update: automatic formatting

--- src/ariel/body_phenotypes/robogen_lite/prebuilt_robots/gecko.py
+++ src/ariel/body_phenotypes/robogen_lite/prebuilt_robots/gecko.py
@@ -1,20 +1,22 @@
+"""TODO(jmdm): description of script."""
+
 from ariel.body_phenotypes.robogen_lite.config import ModuleFaces
 from ariel.body_phenotypes.robogen_lite.modules.brick import BrickModule
 from ariel.body_phenotypes.robogen_lite.modules.core import CoreModule
 from ariel.body_phenotypes.robogen_lite.modules.hinge import HingeModule
-import numpy as np
 
-def gecko():
+
+def gecko() -> CoreModule:
     """
     Create and attach bodies/sites, then print relative orientations between
     abdomen and spine bodies to debug hinge placement/orientation.
 
     Body Description
     ---------
-    The gecko body consists of a core module, 4 legs (flippers), a neck and a spine.
-    For better mobility the front two flippers have 2 hinges (joints), each rotated 90 degrees
-    to each other. Additionally, the back two flippers are rotated 45 degrees compared to the 
-    bode, to encourage forward movement.
+    The gecko body consists of a core module, 4 legs (flippers), a neck and a
+    spine. For better mobility the front two flippers have 2 hinges (joints),
+    each rotated 90 degrees to each other. Additionally, the back two flippers
+    are rotated 45 degrees compared to the bode, to encourage forward movement.
     """
 
     core = CoreModule(
@@ -23,18 +25,18 @@ def gecko():
     neck = HingeModule(
         index=1,
     )
-    # neck.rotate(-90)
+
     abdomen = BrickModule(
         index=2,
     )
     spine = HingeModule(
         index=3,
     )
-    # spine.rotate(90)
+
     butt = BrickModule(
         index=4,
     )
-    
+
     fl_leg = HingeModule(
         index=5,
     )
@@ -43,11 +45,11 @@ def gecko():
         index=15,
     )
     fl_leg2.rotate(90)
-    
+
     fl_flipper = BrickModule(
         index=6,
     )
-    
+
     fr_leg = HingeModule(
         index=7,
     )
@@ -56,7 +58,7 @@ def gecko():
         index=17,
     )
     fr_leg2.rotate(90)
-    
+
     fr_flipper = BrickModule(
         index=8,
     )
@@ -97,7 +99,7 @@ def gecko():
         body=fl_leg.body,
         prefix="fl_leg",
     )
-    
+
     fl_leg.sites[ModuleFaces.FRONT].attach_body(
         body=fl_leg2.body,
         prefix="fl_flipper",
@@ -106,12 +108,12 @@ def gecko():
         body=fl_flipper.body,
         prefix="fl_flipper2",
     )
-    
+
     core.sites[ModuleFaces.RIGHT].attach_body(
         body=fr_leg.body,
         prefix="fr_leg",
     )
-    
+
     fr_leg.sites[ModuleFaces.FRONT].attach_body(
         body=fr_leg2.body,
         prefix="fr_flipper",
@@ -120,7 +122,7 @@ def gecko():
         body=fr_flipper.body,
         prefix="fr_flipper2",
     )
-    
+
     butt.sites[ModuleFaces.LEFT].attach_body(
         body=bl_leg.body,
         prefix="bl_leg",
