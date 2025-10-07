0edfa91d54da287725e6057cb7850c23182986bb
Thomas Schuller <thoschuller@gmail.com>
2025-10-07 18:50:13 +0200

    Revert "bugfix"
    This reverts commit 51bde041c7605c1b91e4e9fb2db03cf416dcbf1b.

--- experiment/A3_np_net.py
+++ experiment/A3_np_net.py
@@ -208,7 +208,7 @@ def initialize_world_and_robot():
     model = world.spec.compile()
     data = mujoco.MjData(model)
     # geoms = world.spec.worldbody.find_all(mujoco.mjtObj.mjOBJ_GEOM)
-    mujoco.mj_resetData(model, data)
+    mj.mj_resetData(model, data)
     return model, data, world
 
 def run_bot_session(weights: np.ndarray, method: str, options: dict = None) -> Tracker:
