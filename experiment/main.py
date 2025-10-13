01ae21beac32088f068d2279c83cfa19afae9bbe
Thomas Schuller <me@tschuller.nl>
2025-10-13 15:15:39 +0200

    various changes

--- experiment/main.py
+++ experiment/main.py
@@ -1,19 +1,21 @@
 import constants
 from terminal import console, progress
-from utils import plot_saved_phenotype, plot_and_record_saved_phenotype
+from utils import plot_saved_phenotype, plot_and_record_saved_phenotype, run_saved_phenotype
 from brain_train import train_individual_from_files
 from body_evolver import body_evolution
 
 if __name__ == "__main__":
     import argparse
     import sys
+    
+    default_duration = constants.STAGE_SETTINGS["FULL"]["DURATION"]
 
-    parser = argparse.ArgumentParser(description="Plot, record, or evolve phenotypes.")
+    parser = argparse.ArgumentParser(description="Plot, record, or evolve Silly Walkers.")
     parser.add_argument("--evolve", choices=["body", "brain"], help="Evolve mode: 'body' or 'brain'.")
     parser.add_argument("--brain", type=str, help="Path to the brain .npy file (for plotting/recording).")
     parser.add_argument("--body", type=str, help="Path to the body .json file (for plotting/recording or brain evolution).")
     parser.add_argument(
-        "--duration", type=float, default=constants.STAGE_SETTINGS["FULL"]["DURATION"], help="Duration of the simulation."
+        "--duration", type=float, default=default_duration, help="Duration of the simulation."
     )
     parser.add_argument(
         "--video",
@@ -24,24 +26,43 @@ if __name__ == "__main__":
             "--plot",
             action="store_true",
             default=False,
-            help="Plot the phenotype (default action if no other action is specified)."
+            help="Plot the phenotype's path and fitness."
         )
     parser.add_argument(
-        "--method",
-        type=str,
-        default="headless",
-        help="Method for running the session (record, headless, etc.).",
+        "--viewer",
+        action="store_true",
+        help="Show the specified brain and body in a viewer.",
     )
 
     args = parser.parse_args()
     
     sys.stdout.flush()
     progress.start()
+    
+    status = 0
 
     try:       
         
-        console.rule("Starting Ministry of Silly Walks AI...")
+        console.rule("Starting Ministry of Silly Walks EA...")
         
+        if [args.evolve, args.viewer, args.plot, args.video].count(True) > 1:
+            raise ValueError("Only one of --evolve, --viewer, --plot, or --video can be set at a time.")
+
+        if args.evolve or args.viewer:
+            if args.duration != default_duration:
+                console.log("[yellow] [WARNING] --duration is ignored in this mode.")
+                
+        if args.viewer:
+            if not args.brain or not args.body:
+                console.log("[red] [ERROR] --brain and --body must be set for viewer mode.")
+                sys.exit(1)
+            console.log("Launching viewer...")
+            run_saved_phenotype(
+                brain_file=args.brain,
+                body_file=args.body,
+                method="viewer",
+            )
+            console.log("Viewer session ended.")
         
         if args.evolve == "body":
             console.log("Running body evolution...")
@@ -66,7 +87,7 @@ if __name__ == "__main__":
             # weights is optional
             result = train_individual_from_files(body_file=args.body, weights_file=args.brain, record_batch=True)
             console.log(f"result: {result}")
-        else:
+        elif args.plot or args.video:
             if not args.brain or not args.body:
                 console.log("[red] [ERROR] --brain and --body must be set for plotting/recording.")
                 sys.exit(1)
@@ -83,14 +104,21 @@ if __name__ == "__main__":
                     brain_file=args.brain,
                     body_file=args.body,
                     duration=args.duration,
-                    method=args.method,
+                    method="headless",
                 )
                 console.log("Plotting completed.")
+        else:
+            # show help if no arguments are provided
+            if len(sys.argv) == 1:
+                parser.print_help(sys.stderr)
+                status = 1
         sys.stdout.flush()
     except Exception as e:
-        console.log(f"[red] [ERROR] Exception in main: {e}")
+        console.log(f"[red] [ERROR] Exception in Evolver: {e}")
         import traceback
         traceback.print_exc()
         sys.stdout.flush()
+        status = 1
     finally:
         progress.stop()
+        exit(status)
