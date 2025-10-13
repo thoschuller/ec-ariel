97d17f0a3778b63a1668a002998991422ad48e18
Thomas Schuller <me@tschuller.nl>
2025-10-13 15:16:05 +0200

    various changes

--- experiment/main.py
+++ experiment/main.py
@@ -1,8 +1,5 @@
 import constants
 from terminal import console, progress
-from utils import plot_saved_phenotype, plot_and_record_saved_phenotype, run_saved_phenotype
-from brain_train import train_individual_from_files
-from body_evolver import body_evolution
 
 if __name__ == "__main__":
     import argparse
@@ -46,52 +43,59 @@ if __name__ == "__main__":
         console.rule("Starting Ministry of Silly Walks EA...")
         
         if [args.evolve, args.viewer, args.plot, args.video].count(True) > 1:
-            raise ValueError("Only one of --evolve, --viewer, --plot, or --video can be set at a time.")
-
+            console.log("Only one of --evolve, --viewer, --plot, or --video can be set at a time.")
+            status = 1
+                
         if args.evolve or args.viewer:
             if args.duration != default_duration:
                 console.log("[yellow] [WARNING] --duration is ignored in this mode.")
-                
-        if args.viewer:
-            if not args.brain or not args.body:
-                console.log("[red] [ERROR] --brain and --body must be set for viewer mode.")
-                sys.exit(1)
-            console.log("Launching viewer...")
-            run_saved_phenotype(
-                brain_file=args.brain,
-                body_file=args.body,
-                method="viewer",
-            )
-            console.log("Viewer session ended.")
         
-        if args.evolve == "body":
-            console.log("Running body evolution...")
-            full_evolve_task = progress.add_task("Full Evolution Progress", total=None)
-            result = body_evolution()
-            fit, _, brain, body_graph = result
-            console.log(f"Body evolution returned fitness: {fit}")
-            console.rule(f"Body evolution completed. Best fitness: {fit}")
-            console.rule(f"Starting prolonged brain training on best body...")
-            brain_result = train_individual_from_files(body_graph=body_graph, np_weights=brain, time_limit=constants.EXTRA_TRAINING_TIME, record_batch=True)
-            console.log(f"Prolonged brain training returned: {brain_result[1]}")
-            console.rule(f"Prolonged brain training completed. Best fitness: {brain_result[1]}")
-            progress.remove_task(full_evolve_task)
+            if args.viewer:
+                if not args.brain or not args.body:
+                    console.log("[red] [ERROR] --brain and --body must be set for viewer mode.")
+                    sys.exit(1)
+                console.log("Launching viewer...")
+                from utils import run_saved_phenotype
+                run_saved_phenotype(
+                    brain_file=args.brain,
+                    body_file=args.body,
+                    method="viewer",
+                )
+                console.log("Viewer session ended.")
             
+            elif args.evolve == "body":
+                console.log("Running body evolution...")
+                full_evolve_task = progress.add_task("Full Evolution Progress", total=None)
+                from body_evolver import body_evolution
+                from brain_train import train_individual_from_files
+                result = body_evolution()
+                fit, _, brain, body_graph = result
+                console.log(f"Body evolution returned fitness: {fit}")
+                console.rule(f"Body evolution completed. Best fitness: {fit}")
+                console.rule(f"Starting prolonged brain training on best body...")
+                brain_result = train_individual_from_files(body_graph=body_graph, np_weights=brain, time_limit=constants.EXTRA_TRAINING_TIME, record_batch=True)
+                console.log(f"Prolonged brain training returned: {brain_result[1]}")
+                console.rule(f"Prolonged brain training completed. Best fitness: {brain_result[1]}")
+                progress.remove_task(full_evolve_task)
+                
+                
+            elif args.evolve == "brain":
+                if not args.body:
+                    console.log("[red] [ERROR] --body must be set for --evolve brain mode.")
+                    sys.exit(1)
+                console.log("Running brain evolution...")
+                from brain_train import train_individual_from_files
+                # weights is optional
+                result = train_individual_from_files(body_file=args.body, weights_file=args.brain, record_batch=True)
+                console.log(f"result: {result}")
             
-        elif args.evolve == "brain":
-            if not args.body:
-                console.log("[red] [ERROR] --body must be set for --evolve brain mode.")
-                sys.exit(1)
-            console.log("Running brain evolution...")
-            from brain_train import train_individual_from_files
-            # weights is optional
-            result = train_individual_from_files(body_file=args.body, weights_file=args.brain, record_batch=True)
-            console.log(f"result: {result}")
         elif args.plot or args.video:
             if not args.brain or not args.body:
                 console.log("[red] [ERROR] --brain and --body must be set for plotting/recording.")
                 sys.exit(1)
             if args.video:
+                console.log("Recording video and plotting path...")
+                from utils import plot_and_record_saved_phenotype, plot_saved_phenotype
                 plot_and_record_saved_phenotype(
                     brain_file=args.brain,
                     body_file=args.body,
@@ -100,6 +104,8 @@ if __name__ == "__main__":
                 )
                 console.log("Recording and plotting completed.")
             else:
+                console.log("Plotting path...")
+                from utils import plot_saved_phenotype
                 plot_saved_phenotype(
                     brain_file=args.brain,
                     body_file=args.body,
